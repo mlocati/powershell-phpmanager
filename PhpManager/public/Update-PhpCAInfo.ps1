955bcdcef97df860040314da0229b0fbe03a9485
Michele Locati <michele@locati.it>
2018-04-26 14:01:55 +0200

    [minor] Fix indentation

--- PhpManager/public/Update-PhpCAInfo.ps1
+++ PhpManager/public/Update-PhpCAInfo.ps1
@@ -61,7 +61,7 @@ function Update-PhpCAInfo() {
             Finally {
                 $streamWriter.Dispose()
             }
-    }
+        }
         Finally {
             $stream.Dispose()
         }
