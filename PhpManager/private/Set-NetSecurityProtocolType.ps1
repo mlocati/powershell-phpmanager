1a86180a22e6e55a0470e985ae2a9ce73a8e2a6a
Michele Locati <michele@locati.it>
2019-05-17 15:02:32 +0200

    Check (and set) the SchUseStrongCrypto registry key

--- PhpManager/private/Set-NetSecurityProtocolType.ps1
+++ PhpManager/private/Set-NetSecurityProtocolType.ps1
@@ -10,10 +10,34 @@
     begin {
     }
     process {
+        $regValueName = 'SchUseStrongCrypto'
+        try {
+            $netVersion = [System.Environment].Assembly.ImageRuntimeVersion
+            $regKeyPath = "HKLM:\SOFTWARE\Microsoft\.NETFramework\$netVersion"
+            if (Test-Path -LiteralPath $regKeyPath) {
+                $regKey = Get-Item -LiteralPath $regKeyPath
+                if (-Not($regKey.GetValue($regValueName))) {
+                    Write-Verbose "Setting the $regValueName registry key"
+                    Set-ItemProperty -LiteralPath $regKeyPath -Name $regValueName -Value 1
+                }
+                $regKeyPath = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\$netVersion"
+                if (Test-Path -LiteralPath $regKeyPath) {
+                    $regKey = Get-Item -LiteralPath $regKeyPath
+                    if (-Not($regKey.GetValue($regValueName))) {
+                        Write-Verbose "Setting the $regValueName registry key for WOW64"
+                        Set-ItemProperty -LiteralPath $regKeyPath -Name $regValueName -Value 1
+                    }
+                }
+            } else {
+                Write-Verbose "Failed to find the registry entry for the .NET Runtime version $netVersion"
+            }
+        } catch [System.Exception] {
+            Write-Verbose ("Error while configuring the {0} registry key: {1}" -f $regValueName,$_.Exception.Message)
+        }
         try {
             [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 + [Net.SecurityProtocolType]::Tls11 + [Net.SecurityProtocolType]::Tls
         } catch {
-            Write-Debug '[Net.ServicePointManager] or [Net.SecurityProtocolType] not found in current environment'
+            Write-Verbose '[Net.ServicePointManager] or [Net.SecurityProtocolType] not found in current environment'
         }
     }
     end {
