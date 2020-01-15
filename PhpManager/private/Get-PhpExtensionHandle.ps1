e8f894778f342da886cea3598baa3f3673daf85e
Serghei Iakovlev <sergeyklay@users.noreply.github.com>
2020-01-15 15:44:16 +0200

    Added Zephir Parser support

--- PhpManager/private/Get-PhpExtensionHandle.ps1
+++ PhpManager/private/Get-PhpExtensionHandle.ps1
@@ -34,6 +34,7 @@
             'nt user api' { $handle = 'ntuser' }
             'the ioncube php loader' { $handle = 'ioncube_loader' }
             'ioncube loader' { $handle = 'ioncube_loader' }
+            'zephir parser' { $handle = 'zephir_parser' }
             default {
                 if (-Not($handle -match '^[a-z0-9][a-z0-9_\-]+$')) {
                     throw "Unrecognized PHP extension name: $Name"
