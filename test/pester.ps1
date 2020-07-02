127e0487c034e4c2f8f1424ce8543be77e8d4c79
Michele Locati <michele@locati.it>
2020-07-02 17:31:50 +0200

    Force Pester version ^4.5

--- test/pester.ps1
+++ test/pester.ps1
@@ -2,7 +2,7 @@
 
 $Global:ProgressPreference = 'SilentlyContinue'
 
-Import-Module -Name Pester -MinimumVersion 4.5
+Import-Module -Name Pester -MinimumVersion 4.5 -MaximumVersion 4.9999.9999
 
 New-Variable -Scope Global -Option ReadOnly -Name PHPMANAGER_MODULEPATH -Value (Split-Path -LiteralPath $PSScriptRoot | Join-Path -ChildPath PhpManager) -Force
 
