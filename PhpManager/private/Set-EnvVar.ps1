a1820603fac4619e9d205b340e3437dcebdffb90
Michele Locati <michele@locati.it>
2022-02-12 19:15:40 +0100

    Add Set-OpenSSLConf function (#97)
    * Extract functions from Edit-FolderInPath
    
    * Add Set-OpenSSLConf function

---
+++ PhpManager/private/Set-EnvVar.ps1
@@ -0,0 +1,89 @@
+﻿function Set-EnvVar() {
+    <#
+    .Synopsis
+    Set an environment variable, for the current process/user/machine
+
+    .Parameter Name
+    The name of the environment variable
+
+    .Parameter Value
+    The value of the environment variable
+
+    .Parameter Process
+    Update the environment variable for the current process?
+
+    .Parameter User
+    Update the environment variable for the current user?
+
+    .Parameter Machine
+    Update the environment variable for the local machine?
+    #>
+    [OutputType()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNull()]
+        [ValidateLength(1, [int]::MaxValue)]
+        [string] $Name,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string] $Value,
+        [Parameter(Mandatory = $false, Position = 2)]
+        [ValidateNotNull()]
+        [bool] $Process = $false,
+        [Parameter(Mandatory = $false, Position = 3)]
+        [ValidateNotNull()]
+        [bool] $User = $false,
+        [Parameter(Mandatory = $false, Position = 4)]
+        [ValidateNotNull()]
+        [bool] $Machine = $false,
+        [switch] $NoBroadcast
+    )
+    begin {
+    }
+    process {
+        if ($null -eq $Value) {
+            $Value = ''
+        }
+        If ($Process) {
+            New-Item -Path Env: -Name $Name -Value $Value -Force | Out-Null
+        }
+        if (-not($User -or $Machine)) {
+            return;
+        }
+        $needDirectRegistryAccess = [System.Environment]::GetEnvironmentVariable[0].OverloadDefinitions.Count -lt 2
+        if ($User) {
+            if ($needDirectRegistryAccess) {
+                New-ItemProperty -Path 'HKCU:\Environment' -Name $Name -Value $Value -PropertyType ExpandString -Force | Out-Null
+            } else {
+                [System.Environment]::SetEnvironmentVariable($Name, $Value, 'User') | Out-Null
+            }
+        }
+        if ($Machine) {
+            $currentUser = [System.Security.Principal.WindowsPrincipal] [System.Security.Principal.WindowsIdentity]::GetCurrent()
+            if ($currentUser.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+                if ($needDirectRegistryAccess) {
+                    New-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Session Manager\Environment' -Name $Name -Value $Value -PropertyType ExpandString -Force | Out-Null
+                } else {
+                    [System.Environment]::SetEnvironmentVariable($Name, $Value, 'Machine') | Out-Null
+                }
+            } else {
+                $NameEscaped = $Name -replace "'", "''"
+                $ValueEscaped = $Value -replace "'", "''"
+                if ($needDirectRegistryAccess) {
+                    $exeCommand = "New-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Session Manager\Environment' -Name '$NameEscaped' -Value '$ValueEscaped' -PropertyType ExpandString -Force | Out-Null"
+                } else {
+                    $exeCommand = "[System.Environment]::SetEnvironmentVariable('$NameEscaped', '$ValueEscaped', 'Machine') | Out-Null"
+                }
+                Start-Process -FilePath 'powershell.exe' -ArgumentList "-Command ""$exeCommand""" -WindowStyle Hidden -Verb RunAs -Wait
+            }
+        }
+        if ($needDirectRegistryAccess -and -not($NoBroadcast)) {
+            try {
+                Invoke-BroadcaseEnvChanged
+            } catch {
+                Write-Debug -Message $_
+            }
+        }
+    }
+    end {
+    }
+}
---
+++ PhpManager/public/Set-OpenSSLConf.ps1
@@ -0,0 +1,81 @@
+﻿function Set-OpenSSLConf
+{
+    <#
+    .Synopsis
+    Persist/fix the path of the openssl.cnf file by settng the OPENSSL_CONF environment variable
+
+    .Parameter Path
+    The path to the openssl.cnf file (if not provided we'll try to detect it)
+
+    .Parameter Target
+    'Process' [default] to set the OPENSSL_CONF environment variable for the current process only
+    'User' to set the OPENSSL_CONF environment variable for the current process and the current user
+    'Machine' to set the OPENSSL_CONF environment variable for the current process and the local machine
+
+    .Example
+    Set-OpenSSLConf C:\Path\to\openssl.cnf
+    #>
+    [OutputType()]
+    param (
+        [Parameter(Mandatory = $false, Position = 0, HelpMessage = 'The path of openssl.cnf')]
+        [string] $Path,
+        [Parameter(Mandatory = $false, Position = 1, HelpMessage = 'The target level of the OPENSSL_CONF environment variable to be set')]
+        [ValidateNotNull()]
+        [ValidateSet('Process', 'User', 'Machine')]
+        [string] $Target = 'Process'
+    )
+    begin {
+    }
+    process {
+        if ($null -eq $Path -or $Path -eq '') {
+            $searchPaths = @(
+                $Env:OPENSSL_CONF,
+                $Env:SSLEAY_CONF,
+                "${Env:CommonProgramFiles}\SSL\openssl.cnf" + '',
+                "${Env:CommonProgramFiles(x86)}\SSL\openssl.cnf"
+            )
+            foreach ($php in Get-Php) {
+                $searchPaths += "$($php.Folder)\extras\ssl\openssl.cnf"
+                $searchPaths += "$($php.Folder)\extras\openssl\openssl.cnf"
+                $searchPaths += "$($php.Folder)\extras\openssl.cnf"
+            }
+            $absolutePath = ''
+            foreach ($searchPath in $searchPaths) {
+                if (-not($searchPath) -or -not(Test-Path -LiteralPath $searchPath)) {
+                    continue
+                }
+                $searchPath = $(Resolve-Path -LiteralPath $searchPath).Path
+                if (Test-Path -LiteralPath $searchPath -PathType Leaf) {
+                    $absolutePath = $searchPath
+                    break
+                }
+            }
+            if ($absolutePath -eq '') {
+                throw 'Unable to find openssl.cnf'
+                return
+            }
+            Write-Verbose "openssl.cnf found at $absolutePath"
+        } else {
+            if (Test-Path -LiteralPath $Path -PathType Container) {
+                $tmp = Join-Path -Path $Path -ChildPath 'openssl.cnf'
+                if (-not(Test-Path -LiteralPath $tmp -PathType Leaf)) {
+                    throw "The folder $Path does not contain the file openssl.cnf"
+                    return;
+                }
+                $Path = $tmp
+            } elseif (-not(Test-Path -LiteralPath $Path -PathType Leaf)) {
+                throw "Unable to find the file or directory $Path"
+                return;
+            }
+            $absolutePath = $(Resolve-Path -LiteralPath $Path).Path
+        }
+        $Env:OPENSSL_CONF = $absolutePath
+        if ($Target -eq 'User') {
+            Set-EnvVar -Name 'OPENSSL_CONF' -Value $absolutePath -User $true
+        } elseif ($Target -eq 'Machine') {
+            Set-EnvVar -Name 'OPENSSL_CONF' -Value $absolutePath -Machine $true
+        }
+    }
+    end {
+    }
+}
